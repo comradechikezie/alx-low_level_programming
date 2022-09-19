21473e881d5001cbc34318cc63a124b10c2aec52
Chikezie <comradechikezie4ilea@gmail.com>
2022-09-19 15:46:12 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void reset_to_98(int *n);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s)
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+int _atoi(char *s);
+
+#endif
