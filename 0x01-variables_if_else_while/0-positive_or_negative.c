586c669ebb53773e246fa3ee7216655852d00ec4
Chikezie <comradechikezie4ilea@gmail.com>
2022-09-18 17:14:24 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,13 +16,13 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
-	
+
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
-	
+
 		printf("%d is negetive\n", n);
-	
+
 
 	return (0);
 }
