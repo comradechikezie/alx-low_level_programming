d8473aca5dd9f2ccf3ff99dfc9c90c6d42ebac13
Chikezie <comradechikezie4ilea@gmail.com>
2022-09-18 18:24:23 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,9 +13,13 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
+	{
 		putchar(low);
+	}
 	for (low = 'A'; low <= 'z'; low++)
+	{
 		putchar(low);
+	}
 	putchar('\n');
 
 	return (0);
